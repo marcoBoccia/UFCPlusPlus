a72da73b92f6326def7bfdc72f3562462cfcde84
Marco Boccia <pokermark95@live.it>
2023-02-13 14:02:50 +0100

    svolto esercizio 9

---
+++ es9.cpp
@@ -0,0 +1,31 @@
+// Stampa tabellina del 7 , (stampato tavola pitagorica )
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+// ! Tabellina del 7
+int main(){
+    int n = 7;
+    for(int i = 0; i <= 10; i++){
+        cout<<n*i<<endl;
+    }
+}
+
+
+
+// ! Tavola pitagorica
+/* 
+int main(){
+	int i,j;
+        const int n=10;
+	
+	for(i=1; i<=n; i++) {
+		for(j=1; j<=n; j++)
+			cout<<"\t"<<i*j;		
+		cout<<endl;
+	}
+        return 0;
+}
+*/
