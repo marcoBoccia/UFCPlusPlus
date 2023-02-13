86def0b4e51049fa8419afd9d3deecb50093f528
Marco Boccia <pokermark95@live.it>
2023-02-13 14:03:42 +0100

    iniziato esercizio 10

---
+++ es10.cpp
@@ -0,0 +1,30 @@
+// Scrivere un programma che stampa i numeri pari da 0 a 20
+
+
+
+/*
+    #include <iostream>
+    #include <string>
+    #include <cmath>
+    using namespace std;
+
+    int n;
+    int i;
+    const int M = 20;
+
+    int stampaPariUnoVenti(int i, int M) {
+        for ( i = 0; i <= M ; i++)
+        {
+        if (i%2==0)
+        {
+            cout << i <<endl;
+        }
+        return 0; 
+        }
+    }
+
+
+    int main(){
+        stampaPariUnoVenti(1,20);
+    }
+*/
