6ca579c58bad8c8793cbbd5341e635a2f98d2572
Marco Boccia <pokermark95@live.it>
2023-02-14 15:01:38 +0100

    finito es16

--- es16.cpp
+++ es16.cpp
@@ -1,5 +1,5 @@
 /* Scrivere un programma che riceva in ingresso un numero positivo N e determini il massimo intero K tale che la somma dei primi K interi sia minore o uguale a N.
-Ad esempio, se N=20 allora K risulta 5, infatti 
+Ad esempio, se N=20 allora K risulta 5, infatti
 1+2+3+4+5=15 mentre
 1+2+3+4+5+6=21
 */
@@ -9,12 +9,16 @@ Ad esempio, se N=20 allora K risulta 5, infatti
 #include <cmath>
 using namespace std;
 
-int main() {
+// solution 1
+/*
+int main()
+{
     int N, K = 0, somma = 0;
     cout << "Inserisci un numero positivo N: ";
     cin >> N;
 
-    while (somma <= N) {
+    while (somma <= N)
+    {
         K++;
         somma += K;
     }
@@ -23,3 +27,25 @@ int main() {
 
     return 0;
 }
+*/
+
+/* solution 2
+int N = 0;
+
+void maxInteger(int N);
+{
+    int K = 0
+    int somma = 0;
+    cout << "Inserisci un numero positivo N: ";
+    cin >> N;
+
+    while (somma <= N)
+    {
+        K++;
+        somma += K;
+    }
+
+    cout << "Il massimo intero K tale che la somma dei primi K interi sia minore o uguale a N e': " << K - 1 << endl;
+
+}
+*/
